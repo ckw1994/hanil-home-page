3e52ed0f71c4667f67823599c217db33aab85d76
cold-miss <97150742+cold-miss@users.noreply.github.com>
2022-03-29 14:06:20 +0900

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -2,127 +2,128 @@
 function header() {
     return `
     <header>
-    <h1 id="H-logo">
-        <a href="./index.html">
-            <img src="./images/한일제약-컬러-logo.svg" alt="color-logo">
-        </a>
-    </h1>
-    <nav id="GNB">
-            <ul class="depth-1">
-                <li>
-                    <a href="#" class="pc-only">회사소개</a>
-                    <button class="m-gnb-up-btn mobile-only">
-                      <p class="mobile-only"> 회사소개 </p>  <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
-                    </button>
-                    <ul class="depth-2">
-                        <li>
-                            <a href="./About-Us.html">기업소개</a>
-                        </li>
-                        <li>
-                            <a href="./CEO-Greetings.html">CEO 인사말</a>
-                        </li>
-                        <li>
-                            <a href="./History.html">연혁</a>
-                        </li>
-                        <li>
-                            <a href="./Location.html">오시는 길</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="pc-only">제품소개</a>
-                    <button class="m-gnb-up-btn mobile-only">
-                    <p class="mobile-only"> 제품소개 </p> <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
-                    </button>
-                    <ul class="depth-2">
-                        <li>
-                            <a href="./product.html"">제품정보</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="pc-only">
-                        채용공고
-                    </a>
-                    <button class="m-gnb-up-btn mobile-only">
-                    <p class="mobile-only"> 채용공고 </p> <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
-                    </button>
-                    <ul class="depth-2">
-                        <li>
-                            <a href="./recruit.html">
-                                채용안내
-                            </a>
-                        </li>
-                        <li>
-                            <a href="./talent.html">
-                                인재상
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="pc-only">
-                        고객문의
-                    </a>
-                    <button class="m-gnb-up-btn mobile-only">
-                    <p class="mobile-only"> 고객문의 </p>  <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
-                    </button>
-                    <ul class="depth-2">
-                        <li>
-                            <a href="./FAQ.html">
-                                FAQ
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                문의하기
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="pc-only">
-                        홍보센터
-                    </a>
-                    <button class="m-gnb-up-btn mobile-only">
-                    <p class="mobile-only"> 홍보센터 </p>  <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
-                    </button>
-                    <ul class="depth-2">
-                        <li>
-                            <a href="#">
-                                공지사항
-                            </a>
-                        </li>                        
-                        <li>
-                            <a href="#">
-                                홍보영상
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-
-            </ul>
-
-        </nav>
-        <div id="Right-menu">
-        <div class="Language-group pc-only">
-            <p class="pc-only">EN</p>
-            <a href="#" class="pc-only">
-                <img src="./images/Language-icon.svg" alt="Language-icon.svg">
+    <div class="header-wrap">
+        <h1 id="H-logo">
+            <a href="./index.html">
+                <img src="./images/한일제약-컬러-logo.svg" alt="color-logo">
             </a>
-            <p class="H-Language-line pc-only">ㅣ</p>
+        </h1>
+        <nav id="GNB">
+                <ul class="depth-1">
+                    <li>
+                        <a href="#" class="pc-only">회사소개</a>
+                        <button class="m-gnb-up-btn mobile-only">
+                        <p class="mobile-only"> 회사소개 </p>  <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
+                        </button>
+                        <ul class="depth-2">
+                            <li>
+                                <a href="./About-Us.html">기업소개</a>
+                            </li>
+                            <li>
+                                <a href="./CEO-Greetings.html">CEO 인사말</a>
+                            </li>
+                            <li>
+                                <a href="./History.html">연혁</a>
+                            </li>
+                            <li>
+                                <a href="./Location.html">오시는 길</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" class="pc-only">제품소개</a>
+                        <button class="m-gnb-up-btn mobile-only">
+                        <p class="mobile-only"> 제품소개 </p> <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
+                        </button>
+                        <ul class="depth-2">
+                            <li>
+                                <a href="./product.html"">제품정보</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" class="pc-only">
+                            채용공고
+                        </a>
+                        <button class="m-gnb-up-btn mobile-only">
+                        <p class="mobile-only"> 채용공고 </p> <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
+                        </button>
+                        <ul class="depth-2">
+                            <li>
+                                <a href="./recruit.html">
+                                    채용안내
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./talent.html">
+                                    인재상
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" class="pc-only">
+                            고객문의
+                        </a>
+                        <button class="m-gnb-up-btn mobile-only">
+                        <p class="mobile-only"> 고객문의 </p>  <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
+                        </button>
+                        <ul class="depth-2">
+                            <li>
+                                <a href="./FAQ.html">
+                                    FAQ
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    문의하기
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" class="pc-only">
+                            홍보센터
+                        </a>
+                        <button class="m-gnb-up-btn mobile-only">
+                        <p class="mobile-only"> 홍보센터 </p>  <img src="./images/m-gnb-up-btn.svg" alt="m-gnb-up-btn.svg">
+                        </button>
+                        <ul class="depth-2">
+                            <li>
+                                <a href="#">
+                                    공지사항
+                                </a>
+                            </li>                        
+                            <li>
+                                <a href="#">
+                                    홍보영상
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <div id="Right-menu">
+            <div class="Language-group pc-only">
+                <p class="pc-only">EN</p>
+                <a href="#" class="pc-only">
+                    <img src="./images/Language-icon.svg" alt="Language-icon.svg">
+                </a>
+                <p class="H-Language-line pc-only">ㅣ</p>
+            </div>
+            <!-- pc-site-map-btn -->
+            <button id="site-map-btn" class="pc-only">
+                <img src="./images/Site-map-btn.svg" alt="Site-map-btn.svg">
+            </button>
+            <!-- mobile-sitme-map-toggle-btn -->
+            <button id="mobile-toggle-btn" class="mobile-only">
+                <img src="./images/m-toggle-btn.svg" alt="m-toggle-btn.svg">
+            </button>
         </div>
-        <!-- pc-site-map-btn -->
-        <button id="site-map-btn" class="pc-only">
-            <img src="./images/Site-map-btn.svg" alt="Site-map-btn.svg">
-        </button>
-        <!-- mobile-sitme-map-toggle-btn -->
-        <button id="mobile-toggle-btn" class="mobile-only">
-            <img src="./images/m-toggle-btn.svg" alt="m-toggle-btn.svg">
-        </button>
     </div>
-</header>
-<div class="site-map pc-only">
+    <div class="site-map pc-only">
         <div class="site-map-logo">
             <a href="./">
                 <img src="./images/한일제약-컬러-logo.svg" alt="한일제약 컬러 -logo.svg">
@@ -170,6 +171,7 @@ function header() {
             <img src="./images/Site-map-close-btn.svg" alt="Site-map-close-btn.svg">
         </button>
     </div>
+</header>
    `
 }
 
